568ca5f02484bfdbd743cf08b22ca5fae6751c13
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-10-29 13:03:46 +0700

    feat: finishing landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Link from "next/link"
-import Image from 'next/image'
+import Link from "next/link";
+import Image from 'next/image';
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background">
-
+    <div className="min-h-screen flex flex-col bg-background">
+      
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <div className="flex items-center space-x-1">
           <Image alt="ProFile logo" src="/images/resume.png" width={40} height={40}/>
@@ -23,8 +23,8 @@ export default function Home() {
         <Button className="bg-primary text-primary-foreground hover:bg-primary/90 text-base font-semibold">Join</Button>
       </nav>
 
-      <main className="container mx-auto px-4 py-16 items-center justify-center">
-        <div className="flex flex-col gap-6  text-center">
+      <main className="container mx-auto px-4 py-16 items-center justify-center flex-grow">
+        <div className="flex flex-col gap-6 text-center">
           <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
             Create Outstanding Resumes,
             <span className="block text-primary">within seconds.</span>
@@ -35,11 +35,22 @@ export default function Home() {
             <span className="block font-bold text-black text-xl"> No guesswork, just result.</span>
           </p>
         </div>
-        <div className="flex flex-row gap-4 justify-center my-10">
+        <div className="flex flex-row space-x-6 items-center justify-center my-10">
           <Button className="bg-primary text-primary-foreground hover:bg-primary/90 font-semibold text-base">Try Profile</Button>
-          <Button variant="ghost" className="text-black hover:underline">Learn More</Button>
+          <Link className="text-lg font-medium hover:underline" href="/about">
+            Learn More
+          </Link>
         </div>
       </main>
+
+      <footer className="container w-full mx-auto px-4 py-4 flex items-center justify-between">
+        <p className="text-xs md:text-sm font-normal text-gray-600 dark:text-gray-300">
+            Built by <Link href="https://x.com/rafiwiranaa" passHref className="underline" aria-label="Visit Rafi's Twitter">Rafi</Link>.
+        </p>
+        <p className="text-xs md:text-sm font-normal text-gray-600 dark:text-gray-300">
+          &copy; {new Date().getFullYear()} ProFile. All rights reserved.
+        </p>
+      </footer>
     </div>
   );
 }
