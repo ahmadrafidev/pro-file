9dec45f4766cbeeb61f97430d6d34572b5e57ad3
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-10-29 12:49:27 +0700

    feat: landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,43 @@
-import Image from "next/image";
+import Link from "next/link"
+import Image from 'next/image'
+
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen bg-background">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <div className="flex items-center space-x-1">
+          <Image alt="ProFile logo" src="/images/resume.png" width={40} height={40}/>
+          <span className="text-3xl font-bold tracking-tighter">ProFile</span>
+        </div>
+        <div className="hidden md:flex space-x-4">
+          <Link className="text-lg font-medium hover:underline" href="/about">
+            About
+          </Link>
+          <Link className="text-lg font-medium hover:underline" href="/pricing">
+            Pricing
+          </Link>
+        </div>
+        <Button className="bg-primary text-primary-foreground hover:bg-primary/90 text-base font-semibold">Join</Button>
+      </nav>
+
+      <main className="container mx-auto px-4 py-16 items-center justify-center">
+        <div className="flex flex-col gap-6  text-center">
+          <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
+            Create Outstanding Resumes,
+            <span className="block text-primary">within seconds.</span>
+          </h1>
+          <p className="max-w-2xl mx-auto text-lg text-gray-600">
+            Stop spending hours crafting the perfect resume. 
+            ProFile AI and proven templates transform your experience into an ATS-friendly resume. 
+            <span className="block font-bold text-black text-xl"> No guesswork, just result.</span>
+          </p>
+        </div>
+        <div className="flex flex-row gap-4 justify-center my-10">
+          <Button className="bg-primary text-primary-foreground hover:bg-primary/90 font-semibold text-base">Try Profile</Button>
+          <Button variant="ghost" className="text-black hover:underline">Learn More</Button>
         </div>
       </main>
     </div>
---
+++ app/pricing/page.tsx
@@ -0,0 +1,7 @@
+export default function Pricing() {
+  return (
+    <div className="min-h-screen bg-background">
+      Pricing Page
+    </div>
+  );
+}
