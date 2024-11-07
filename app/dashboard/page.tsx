08cb6079e64123e19d713dbdab18b9bc8acd4923
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-11-07 20:58:42 +0700

    chore: init new page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Layout from '@/components/Layout/Layout';
+
+export default function DashboardPage() {
+  return (
+    <Layout>
+      <main className="container mx-auto px-4 py-16 items-center justify-center flex-grow">
+        Dashboard User
+      </main>
+    </Layout>
+  )
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,11 @@
+import Layout from '@/components/Layout/Layout';
+
+export default function ProfilePage() {
+  return (
+    <Layout>
+      <main className="container mx-auto px-4 py-16 items-center justify-center flex-grow">
+        Profile User
+      </main>
+    </Layout>
+  )
+}
